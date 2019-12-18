dfe5742ee7119b90c3fe9c5b20632376a2f7d223
Supriya Adep <supriya.adep@fractalanalytics.com>
2019-12-18 12:51:28 +0530

    Updates

---
+++ precache-manifest.96c84686ee3c4f4bb00e638c913f862d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed263a7d781ea51ba1c4b986eeba8282",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b5997f32d7c6429f1d28",
+    "url": "/myapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "16f70b4d044a196c04ee",
+    "url": "/myapp/static/js/2.972c573f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/myapp/static/js/2.972c573f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5997f32d7c6429f1d28",
+    "url": "/myapp/static/js/main.04063501.chunk.js"
+  },
+  {
+    "revision": "145f18835aa2156eb7a2",
+    "url": "/myapp/static/js/runtime-main.6ac6695c.js"
+  }
+]);
